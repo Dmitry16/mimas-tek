6806bddd9e7e044091870b686ca04ef7b38811a6
Dmytro Davydov <dmytro.davydov@cheetahdigital.com>
2024-04-01 17:26:33 +0100

    footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,25 @@
-// Footer component using MUI Typography and Box components:
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Copyright from './Copyright';
 
 const Footer = () => {
     return (
-        <Box sx={{}}>
-            <Copyright />
+        <Box sx={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            mb: 0, 
+            position: 'sticky', 
+            zIndex: 2,
+            bgcolor: '#FFFFFF',
+            bottom: 0, 
+            width: '100%',
+            height: '5em' 
+        }}>
+            <Typography variant="body2" color="text.darkBlue" align="center">
+                Mimas_Tek © {new Date().getFullYear()}.
+            </Typography>
         </Box>
     );
 }
